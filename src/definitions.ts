c852b94c0eca8a992659d7c893303c677c31bc20
Joe Flateau <joe@joeflateau.net>
2021-07-12 15:47:21 -0400

    add types for plugin events

--- src/definitions.ts
+++ src/definitions.ts
@@ -1,8 +1,27 @@
+import { PluginListenerHandle } from "@capacitor/core";
+
+type NoArgEvents =
+  | "playEnd"
+  | "playAllEnd"
+  | "playPaused"
+  | "playNext"
+  | "playPrevious"
+  | "play";
+
+type EventNameDataMap = {
+  playTimeUpdate: { currentTime: number; duration: number };
+} & Record<NoArgEvents, {}>;
+
 export interface AudioPluginPlugin {
   playList(options: { items: PlaylistItem[] }): void;
   setPlaying(info: NowPlayingInfo): void;
   pausePlay(): void;
   resumePlay(): void;
+
+  addListener<EventName extends keyof EventNameDataMap>(
+    eventName: EventName,
+    listener: (data: EventNameDataMap[EventName]) => void
+  ): PluginListenerHandle;
 }
 
 export interface PlaylistItem {
